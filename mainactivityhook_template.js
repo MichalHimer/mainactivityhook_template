a3b2cae84307c6b0eec79bf0d90a6f4fdb98e19d
MichalHimer <122698763+MichalHimer@users.noreply.github.com>
2023-04-20 08:17:11 +0200

    Added mainactivityhook_template.js

---
+++ mainactivityhook_template.js
@@ -0,0 +1,10 @@
+console.log("Script loaded!");
+Java.perform(function(){
+                var mainapp = Java.use("package.Activity");
+                mainapp.methoda.implementation = function(){
+                                console.log("Injection successful!");
+                                var rtrn = /*dopln*/;
+                                return rtrn;
+                };
+                send("Rootdetectioncheckresultsasfalse-ovator!");
+});
